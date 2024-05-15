4f9473aa0e5d8c36583bb8eca4c82ea3321ff430
chernyshovdmitrydev <148445915+chernyshovdmitrydev@users.noreply.github.com>
2024-05-15 23:27:02 +0400

    gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publickPath: `/DataHeroes/`,
+  publicPath: `/DataHeroes/`,
   transpileDependencies: true
 })
